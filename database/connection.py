5555eaaca80e73a9ea80987911dc72e357a32437
exangedpine <alanzihua@gmail.com>
2025-05-15 18:33:49 -0600

    Versión funcional con operaciones crud correctas en con mongo DB

--- database/connection.py
+++ database/connection.py
@@ -1,5 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 import os
 
-client = AsyncIOMotorClient(os.getenv("MONGODB_URI"))
+load_dotenv()  # Esta línea es esencial para cargar las variables del archivo .env
+
+client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
 db = client["BiciTour"]
